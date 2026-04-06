ddcbd0e5f389af62306a1835bd4445d367d33482
Elizabeth Coats <escoats@unc.edu>
2026-04-05 22:23:36 -0400

    Finish first pass implementation

--- go/main.go
+++ go/main.go
@@ -13,7 +13,7 @@ const custArrivMin = 500 // ms
 const custArrivMax = 2000 // ms
 const cutDurMin = 1000 // ms
 const cutDurMax = 4000 // ms
-const satThresh = 3.0 // sec per star lost
+const satThresh = 3000 // ms per star lost
 const gracePeriod = 8000 // ms
 
 // Universal message type
@@ -40,6 +40,8 @@ type Message struct {
 	CustomerID int
 	Value int	// payload
 	ArrivalMs int64 // arrival timestamp
+	Rating int // rating from customer
+	WaitTime int64 // customer wait time (ms)
 	// Waiting Room stats
 	QueueLengthStat int 
 	TurnawayCountStat int
@@ -50,6 +52,7 @@ type Message struct {
 
 }
 
+// Util
 func clamp(lo, hi, v int) int {
     if v < lo {
         return lo
@@ -60,29 +63,79 @@ func clamp(lo, hi, v int) int {
     return v
 }
 
-func shopOwner() {
-	// run program
-
-	// start waiting room
-	// start barber
-
-	// spawn customers at random intervals
-
-	// wait for grace period (2x max haircut duration)
-
-	// send stats request to barber & waiting room, save responses
-	// send shutdown to barber and waiting room
-	// print closing report
+func main() {
+	shopOwner()
 }
 
-func waitingRoom(mailbox chan Message, barberChannel chan Message) {
+func shopOwner() {
+	mailbox := make(chan Message)
+	waitRoomChannel := make(chan Message)
+	barberChannel := make(chan Message, 1)
+	go waitingRoom(waitRoomChannel)
+	go barber(barberChannel, waitRoomChannel)
+
+	for i := 0; i < numCustomers; i++ {
+		go customer(i, waitRoomChannel)
+		time.Sleep(time.Duration(rand.Intn(custArrivMax-custArrivMin)+custArrivMin)*time.Millisecond)
+	}
+	time.Sleep(time.Duration(2*cutDurMax)*time.Millisecond)
+	barberChannel <- Message{
+		Kind: MsgGetStats,
+		From: mailbox,
+	}
+	barberStatsReply := <- mailbox
+	waitRoomChannel <- Message{
+		Kind: MsgGetStats,
+		From: mailbox,
+	}
+	waitRoomStatsReply := <- mailbox
+	
+	waitRoomChannel <- Message{
+		Kind: MsgShutdown,
+	}
+	barberChannel <- Message{
+		Kind: MsgShutdown,
+	}
+
+	total_cust := barberStatsReply.CutCountStat + waitRoomStatsReply.TurnawayCountStat
+	cust_served := barberStatsReply.CutCountStat
+	cust_rejected := waitRoomStatsReply.TurnawayCountStat
+	avg_duration := barberStatsReply.AvgCutDurStat
+	avg_rating := barberStatsReply.AvgRatingStat
+
+	fmt.Println("== CLOSING REPORT ==")
+	fmt.Printf("Total customers arrived: %d\n", total_cust)
+	fmt.Printf("Total customers served: %d\n", cust_served)
+	fmt.Printf("Total customers arrived: %d\n", cust_rejected)
+	fmt.Printf("Total customers arrived: %d\n", avg_duration)
+	fmt.Printf("Total customers arrived: %d\n", avg_rating)
+	fmt.Println("============")
+}
+
+
+func waitingRoom(mailbox chan Message) {
 	// todo: clarify when to send wakeup signal; ensure is sent only once per cycle
 	turnaway_count := 0
 	cust_queue := make([]int, 0, waitCapacity)
 	barberIsSleeping := false
+	var barberChannel chan Message
 	for {
 		msg := <- mailbox
 		switch msg.Kind {
+		case MsgNextCustomer:
+			barberChannel = msg.From
+			if len(cust_queue) == 0 {
+				barberChannel <- Message{
+					Kind: MsgNoneWaiting,
+				}
+			} else {
+				next_customer := cust_queue[0]
+				cust_queue = cust_queue[1:]
+				barberChannel <- Message{
+					Kind: MsgCustomerReady,
+					CustomerID: next_customer,
+				}
+			}
 		case MsgArrive:
 			if len(cust_queue) >= waitCapacity {
 				msg.From <- Message{
@@ -102,20 +155,6 @@ func waitingRoom(mailbox chan Message, barberChannel chan Message) {
 				}
 				// todo: log admission + queue depth here
 			}
-
-		case MsgNextCustomer:
-			if len(cust_queue) == 0 {
-				barberChannel <- Message{
-					Kind: MsgNoneWaiting,
-				}
-			} else {
-				next_customer := cust_queue[0]
-				cust_queue = cust_queue[1:]
-				barberChannel <- Message{
-					Kind: MsgCustomerReady,
-					CustomerID: next_customer,
-				}
-			}
 		
 		case MsgGetStats:
 			msg.From <- Message{
@@ -125,7 +164,7 @@ func waitingRoom(mailbox chan Message, barberChannel chan Message) {
 			}
 		
 		case MsgShutdown:
-			// todo: shutdown
+			return
 		}
 	}
 
@@ -157,7 +196,7 @@ func customer(id int, waitRoomChannel chan Message) {
 			}
 			cutStartTime := time.Now().UnixMilli()
 			fmt.Printf("Starting haircut for customer %d", id)
-			wait := arriveTime - cutStartTime
+			wait := cutStartTime - arriveTime
 			jitter := rand.Intn(3) - 1 // {-1, 0, +1}
 			score := clamp(1, 5, int(5-(wait/satThresh))+jitter)
 			
@@ -168,7 +207,8 @@ func customer(id int, waitRoomChannel chan Message) {
 			}
 			rateReqMsg.From <- Message{
 				Kind: MsgRating,
-				Value: score,
+				Rating: score,
+				WaitTime: wait,
 			}
 			return
 			}
@@ -176,33 +216,65 @@ func customer(id int, waitRoomChannel chan Message) {
 
 	}
 
-// todoo ??? idk man.
-func barber() {
-	// send message to waiting room
-	// todo: use buffered channel
-}
 
+func barber(mailbox chan Message, waitRoomChannel chan Message) {
+	// main "awake" loop for Barber
+	num_completed := 0
+	var avg_duration float64 = 0
+	var avg_rating float64 = 0
 
-// todo: ?? idk
-type BarberState int
-
-const (
-	Asleep BarberState = iota
-	Awake
-)
-
-func (b *BarberState) sleepLoop(mailbox chan Message) {
+	waitRoomChannel <- Message{
+		Kind: MsgNextCustomer,
+		From: mailbox,
+	}
+	
 	for {
-		msg := <-mailbox
+		msg := <- mailbox
 		switch msg.Kind {
-			case MsgWakeUp:
-				return // exit sleep loop, re-enter main
-			case MsgShutdown: 
-				return // todo: shutdown
+		case MsgCustomerReady:
+			msg.From <- Message{
+				Kind: MsgStartHaircut,
+			}
+			cutLength := time.Duration(rand.Intn((cutDurMax-cutDurMin)) + cutDurMin) * time.Millisecond
+			time.Sleep(cutLength)
+			ratingReplyChannel := make(chan Message)
+			msg.From <- Message{
+				Kind: MsgRateRequest,
+				From: ratingReplyChannel,
+			}
+			ratingResponse:= <- ratingReplyChannel
+			num_completed += 1
+			avg_rating = (avg_rating + float64(ratingResponse.Rating)) / float64(num_completed)
+			avg_duration = (avg_duration + float64(ratingResponse.WaitTime)) / float64(num_completed)
+
+		case MsgNoneWaiting:
+			shouldExit := barberSleepLoop(mailbox) // block until sleep loop is exited
+			if shouldExit {
+				return
+			}
+			continue
+		case MsgGetStats:
+			msg.From <- Message{
+				Kind: MsgBarberStatsReply,
+				CutCountStat: num_completed,
+				AvgCutDurStat: avg_duration,
+				AvgRatingStat: avg_rating,
+			}
+			continue
+		case MsgWakeUp:
+			continue
 		}
 	}
 }
 
-func (b *BarberState) awakeLoop(mailbox chan Message) {
-
+func barberSleepLoop(mailbox chan Message) bool {
+	for {
+		msg := <-mailbox
+		switch msg.Kind {
+			case MsgWakeUp:
+				return false // return
+			case MsgShutdown:
+				return true // terminate barber
+		}
+	}
 }
